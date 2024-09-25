d79982f21b71237cf3f68f9845bd61771695beb1
MoroniArgueta <arg20004@byui.edu>
2024-09-25 14:10:41 -0600

    I am still trying to catch up

---
+++ sandbox/one_to_ten/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // See https://aka.ms/new-console-template for more information
+        Console.WriteLine("Hello, World!");
+
+        for ( int i = 1; i <= 10; i += 1)
+        {
+            Console.WriteLine{i};
+        }
+
+    }
+}
