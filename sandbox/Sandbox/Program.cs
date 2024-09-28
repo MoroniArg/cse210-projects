1ffa4ce0776d80a2ca93266977831f254cd71476
MoroniArgueta <arg20004@byui.edu>
2024-09-27 20:24:45 -0600

    Working on it

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Sandbox World!");
+        List<int> numberList ; 
+        numberList = new();
+
+        int input=-1;
+        while (input != 0)
+        {
+            Console.Write("Enter something: ");
+            string inputString = Console.ReadLine();
+            input = int.Parse(inputString);
+            if (input != 0)
+            {
+                numberList.Add(input);
+            }
+            
+        }
+
+        foreach (int item in numberList)
+        {
+            Console.WriteLine(item);
+        }
     }
 }
