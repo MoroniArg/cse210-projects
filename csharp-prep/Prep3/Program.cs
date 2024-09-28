9916c90dba7843df8ed591a2601b210b3a8b05cb
MoroniArgueta <arg20004@byui.edu>
2024-09-27 21:24:40 -0600

    Completed Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,31 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int userGuess = -1;
+        Random RandomGenerator = new Random();
+        int MagicNumber = RandomGenerator.Next(0,100);
+
+        while (MagicNumber != userGuess)
+        {
+            Console.Write("What is your guess? ");
+            userGuess = int.Parse(Console.ReadLine());
+            
+
+            if (userGuess < MagicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+
+            else if (userGuess > MagicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        }
+
     }
 }
