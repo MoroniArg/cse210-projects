abbb69084f45fd6a7b26c11291807abcf57c74af
MoroniArgueta <arg20004@byui.edu>
2024-09-27 23:22:54 -0600

    Completed Prep 5, but for some reason, it does not run, even when copy pasting the sample code

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,56 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+        DisplayWelcome();
+        string userName = PromptUserName();
+        int userNumber = PromptUserNumber();
+        int userSquared = SquareNumber(userNumber);
+        DisplayResult(userName, userSquared);
+    }
+        
+    static void DisplayWelcome()
+    {
+        //No return
+        //No integers         
+            
+        Console.WriteLine("Welcome to the Program!");
+    }
+
+    static string PromptUserName()
+    {
+        //Return the user's name
+        //No integers
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        return name;
+    }
+
+    static int PromptUserNumber()
+    {
+        //Return the user's favorite number
+        //No integers
+
+        Console.Write("Please enter your favorite number: ");
+        int number = int.Parse(Console.ReadLine());
+
+        return number;
+    }
+
+    static int SquareNumber(int number)
+    {
+        //Return Number squared
+        //An integer number
+
+        int squared = number * number;
+
+        return squared;
+
+    }
+
+    static void DisplayResult(string name, int square)
+    {
+        Console.WriteLine($"{name}, the square of your number is {square}");
     }
 }
