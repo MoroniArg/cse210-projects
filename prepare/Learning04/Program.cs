407e44ddf42f500d09973df3c347c9e512c8821d
MoroniArgueta <arg20004@byui.edu>
2024-11-01 23:41:46 -0600

    Finished Inheritance Learning Activity Week 7

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,15 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        Assignment a1 = new Assignment("William Rippetoe", "Scales");
+        Console.WriteLine(a1.GetSummary());
+
+        MathAssignment a2 = new MathAssignment("Brianna Riconcillo", "Multiplications", "6", "1-3");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Catarino Manqueros", "US History", "The Civil War");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
